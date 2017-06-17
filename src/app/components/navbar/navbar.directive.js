3298a87aae71abf2bf174f5743354856bc9687c1
jbcharrier <jbcharrier33@gmail.com>
2017-06-17 20:19:17 +0200

    bugfix on show user in navbar

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -5,7 +5,7 @@ export function NavbarDirective() {
     restrict: 'E',
     templateUrl: 'app/components/navbar/navbar.html',
     scope: {
-        creationDate: '='
+      creationDate: '='
     },
     controller: NavbarController,
     controllerAs: 'vm',
@@ -18,7 +18,7 @@ export function NavbarDirective() {
 class NavbarController {
   constructor ($rootScope, $scope, $state, moment) {
     'ngInject';
-
+    
     // "this.creationDate" is available by directive option "bindToController: true"
     this.relativeDate = moment(this.creationDate).fromNow();
     $scope.newUser = sessionStorage.getItem('user');
